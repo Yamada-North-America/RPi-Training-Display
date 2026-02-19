b531bc4d7605ec68f98f4df795e3e4f2398f4d5f
Sourve <zombified7@gmail.com>
2026-02-19 11:10:41 -0500

    Random int value to replace sensor reading

--- Training-Display-Debug.py
+++ Training-Display-Debug.py
@@ -32,6 +32,7 @@ import configparser
 import os
 import mpv
 import time
+import random
 import tkinter as tk
 import lib16inpind as inp16
 
@@ -153,7 +154,8 @@ def combine_layers(layers):
 #Updates sensorValue global variables by reading all 3 layers
 def update():
     global sensorValue
-    sensorValue = combine_layers([inp16.readAll(0), inp16.readAll(1), inp16.readAll(2)])
+    #sensorValue = combine_layers([inp16.readAll(0), inp16.readAll(1), inp16.readAll(2)])
+    sensorValue = random.randint(0, 10)
     #print("Dec: ", f'{sensorValue:016d}', "Bin: ", f'{sensorValue:048b}', end='\r')
 
 #The main function that continuously checks the sensor value and displays the corresponding video or image
