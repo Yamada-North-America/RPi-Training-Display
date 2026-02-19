4d3f8b69807c7abb2dc9d2b6fde42e5b57868433
Sourve <zombified7@gmail.com>
2026-02-19 11:50:44 -0500

    DEBUG: Add debug functionality to update sensorValue based on debugTime

--- Training-Display-Debug.py
+++ Training-Display-Debug.py
@@ -38,6 +38,7 @@ import lib16inpind as inp16
 
 #Global Variables - Adjust video paths here
 sensorValue = 0 #Initial sensor value - Do not change
+debugTime = 0
 # Load paths from configuration file
 config = configparser.ConfigParser()
 config.read('/mnt/Training/RPi-Training-Display/config.ini')
@@ -157,7 +158,11 @@ def combine_layers(layers):
 def update():
     global sensorValue
     #sensorValue = combine_layers([inp16.readAll(0), inp16.readAll(1), inp16.readAll(2)])
-    sensorValue = random.randint(0, 1)
+    sensorValue = 0
+    if debugTime > 100:
+        sensorValue = 1
+    else:
+        debugTime += 1
     #print("Dec: ", f'{sensorValue:016d}', "Bin: ", f'{sensorValue:048b}', end='\r')        
 
 #The main function that continuously checks the sensor value and displays the corresponding video or image
